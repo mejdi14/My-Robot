be324ef9d7cb4ce52fdb93aa07a16ba23d041377
mejdi14 <mejdiclubiste@gmail.com>
2021-02-04 15:21:36 +0100

    create options list

--- index.js
+++ index.js
@@ -2,32 +2,18 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+const options = require('./lib/options');
+const logo = require('./lib/my_robot_logo');
 const joke = require('./lib/jokes');
 const questions = require('./lib/questions');
 const { exec } = require("child_process");
-var List = require('term-list');
+
 clear();
 
-console.log(
-
-    "                                       \n"+
-   "    ▐▀▄      ▄▀▌   ▄▄▄▄▄▄▄             \n"+
-    "   ▌  ▀▄▄▄▄▄▀  ▐▄▀▀ ██ ██ ▀▀▄          \n"+
-   "   ▐    ▀ ▀ ▀                 ▀▄        \n"+
-   "  ▌               ▄            ▀▄      \n"+
-   " ▀█   █▌  █  ▐█   ▀               ▌     \n"+
-   " ▀▌      ▀ ▀      ▀▀              ▐   ▄▄\n"+
-   "▐                                 ▌▄█ █\n"+
-   " ▐                                 █ █▀ \n"+
-   "▐                                 █▀   \n"+
-   "▐                                 ▌    \n"+
-   " ▌                               ▐     \n"+
-   " ▐                               ▌     \n"+
-   "  ▌                             ▐      \n"+
-   "  ▐▄                           ▄▌      \n"+
-   "    ▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀        \n")
+logo.showRobotFace();
+options.showOptionsList();
   
-   var package = ""
+ /*  var package = ""
    exec("adb devices", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
@@ -38,12 +24,12 @@ console.log(
         return;
     }
     
-});
-joke.printAJoke();
+});*/
+//joke.printAJoke();
 
    //questions.whoAreYou()
 
-  exec("adb shell input keyevent 224")
+  /*exec("adb shell input keyevent 224")
   setTimeout(() => {  
   exec("adb shell input swipe 100 1000 900 1000")}, 1000);
   
@@ -59,34 +45,12 @@ joke.printAJoke();
         return;
     }
     package = `${stdout.split("\n")[1].split(":")[1]}`;
-});
+});*/
 
 
 
-var list = new List({ marker: '\033[36m› \033[0m', markerLength: 2 });
-list.add('http://google.com', 'Google');
-list.add('http://yahoo.com', 'Yahoo');
-list.add('http://cloudup.com', 'Cloudup');
-list.add('http://github.com', 'Github');
-list.start();
 
-list.on('keypress', function(key, item){
-  switch (key.name) {
-    case 'return':
-      exec('open ' + item);
-      list.stop();
-      console.log('opening %s', item);
-      break;
-    case 'backspace':
-      list.remove(list.selected);
-      break;
-  }
-});
-
-list.on('empty', function(){
-  list.stop();
-});
-setTimeout(() => {  
+/*setTimeout(() => {  
 exec(`adb shell monkey -p ${package} -c android.intent.category.LAUNCHER 1`)}, 1000);
 setTimeout(() => {  
-exec(`adb shell am start -a android.intent.action.CALL -d tel:25200660`)}, 1000);
+exec(`adb shell am start -a android.intent.action.CALL -d tel:25200660`)}, 1000);*/
