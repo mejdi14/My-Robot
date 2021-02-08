d6a2b5ed3f00a6a78a9e375098736ef83664c6bb
mejdi14 <mejdiclubiste@gmail.com>
2021-02-08 20:40:17 +0100

    save changes

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const chalk = require('chalk');
 const clear = require('clear');
-const figlet = require('figlet');
-const files = require('./lib/files');
-const options = require('./lib/options');
 const logo = require('./lib/my_robot_logo');
 const mobile = require('./lib/open_phone');
 const weather = require('./lib/weather');
 const joke = require('./lib/jokes');
-const questions = require('./lib/questions');
-const {exec} = require("child_process");
-
 clear();
 
-//options.showOptionsList();
 
 (async function () {
     await weather.getWeatherApi();
