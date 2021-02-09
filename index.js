772245c8b20c6f89ab62d386266c4b39d5ead4cb
mejdi14 <mejdiclubiste@gmail.com>
2021-02-09 10:11:17 +0100

    get result from codewars api

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const logo = require('./lib/my_robot_logo');
 const mobile = require('./lib/open_phone');
 const weather = require('./lib/weather');
 const joke = require('./lib/jokes');
+const code_wars = require('./lib/code_wars');
 clear();
 (async function () {
-    await weather.getWeatherApi();
+    await code_wars.checkForCodeWarsTasks()
+ /*   await weather.getWeatherApi();
     await joke.printAJoke();
     await mobile.openPhoneAndSwipeToUnlock();
-    logo.showRobotFace();
+    logo.showRobotFace();*/
 })();
 
 
