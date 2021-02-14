02b4c0c7f30314e4c95d0b97f68c23ddb329e9ab
mejdi14 <mejdiclubiste@gmail.com>
2021-02-15 00:08:30 +0100

    i am making a progress

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const mobile = require('./lib/open_phone');
 const weather = require('./lib/weather');
 const joke = require('./lib/jokes');
 const code_wars = require('./lib/code_wars');
+const life_goals = require('./lib/life_goals');
 const sites_blocker = require('./lib/sites_blocker');
 const ora = require('ora');
 const chalk = require('chalk');
-
+var inquirer = require('inquirer');
 //const spinner = ora(`Loading ${chalk.red('unicorns')}`).start();
 
 
 //clear();
 (async function () {
-    await sites_blocker.checkIfAlreadyBlocked().then(function (result) {
+/*     await sites_blocker.checkIfAlreadyBlocked().then(function (result) {
         console.log("your result is " + result)
     })
-     var hasBeenDone = await code_wars.checkForCodeWarsTasks()
-     console.log(hasBeenDone)
+     var hasBeenDone = await code_wars.checkForCodeWarsTasks() */
+    // console.log(hasBeenDone)
+     await life_goals.showQuestions()
     /*   await weather.getWeatherApi();
        await joke.printAJoke();
        await mobile.openPhoneAndSwipeToUnlock();
