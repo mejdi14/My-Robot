860bbaf307601855f1b93ec78dfaf0575821905b
mejdi14 <mejdiclubiste@gmail.com>
2021-02-15 14:15:58 +0100

    add mongo dependencie

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const logo = require('./lib/my_robot_logo');
 const mobile = require('./lib/open_phone');
 const weather = require('./lib/weather');
 const joke = require('./lib/jokes');
+const send_sms = require('./lib/sms_sender');
 const code_wars = require('./lib/code_wars');
 const life_goals = require('./lib/life_goals');
 const sites_blocker = require('./lib/sites_blocker');
 const ora = require('ora');
 const chalk = require('chalk');
 var inquirer = require('inquirer');
-//const spinner = ora(`Loading ${chalk.red('unicorns')}`).start();
-
 
 //clear();
 (async function () {
@@ -19,7 +18,8 @@ var inquirer = require('inquirer');
     })
      var hasBeenDone = await code_wars.checkForCodeWarsTasks() */
     // console.log(hasBeenDone)
-     await life_goals.showQuestions()
+    // await life_goals.showQuestions()
+    //await send_sms.sendSms()
     /*   await weather.getWeatherApi();
        await joke.printAJoke();
        await mobile.openPhoneAndSwipeToUnlock();
