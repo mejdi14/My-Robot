db07988904fbf2efd6e0fbf4ca1e05e7f48dadaf
mejdi14 <mejdiclubiste@gmail.com>
2021-02-15 16:31:08 +0100

    add new constant

--- database/mongo_db.js
+++ database/mongo_db.js
@@ -1,4 +1,5 @@
 const {MongoClient} = require('mongodb');
+const constants = require('../helpers/constants');
 
 module.exports = {
     async getData(){
@@ -6,12 +7,10 @@ module.exports = {
          * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
          * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
          */
-        const uri = "mongodb+srv://mejdi14:azma4everfuck14@cluster0.txec9.mongodb.net/test?retryWrites=true&authSource=admin&w=majority";
+        const uri = constants.mongodb_uri;
         const client = new MongoClient(uri);
         try {
-            // Connect to the MongoDB cluster
             await client.connect();
-            // Make the appropriate DB calls
             await  listDatabases(client);
         } catch (e) {
             console.error(e);
--- helpers/constants.js
+++ helpers/constants.js
@@ -1,5 +1,7 @@
 const localhost_ip = "172.0.0.1"
+const mongodb_uri = "mongodb+srv://mejdi14:azma4everfuck14@cluster0.txec9.mongodb.net/test?retryWrites=true&authSource=admin&w=majority"
 module.exports =
 {
-    localhost_ip : localhost_ip
+    localhost_ip : localhost_ip,
+    mongodb_uri : mongodb_uri
 }
