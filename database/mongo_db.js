4cc2406090f508fc503dab82964998ec082ebc89
mejdi14 <mejdiclubiste@gmail.com>
2021-02-16 13:50:44 +0100

    add a daily meeting for my tasks

--- database/mongo_db.js
+++ database/mongo_db.js
@@ -1,17 +1,14 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const constants = require('../helpers/constants');
 
 module.exports = {
-    async getData(){
-        /**
-         * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-         * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-         */
+    async getData() {
+
         const uri = constants.mongodb_uri;
         const client = new MongoClient(uri);
         try {
             await client.connect();
-            await  listDatabases(client);
+            await listDatabases(client);
         } catch (e) {
             console.error(e);
         } finally {
@@ -20,8 +17,7 @@ module.exports = {
     }
 }
 
-async function listDatabases(client){
+async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    databasesList.databases.forEach(db => console.log(` - ${db.getData}`));
 };
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const joke = require('./lib/jokes');
 const mongo = require('./database/mongo_db');
 const send_sms = require('./lib/sms_sender');
 const code_wars = require('./lib/code_wars');
+const todo_table = require('./lib/todo_table');
 const life_goals = require('./lib/life_goals');
+const daily_meeting = require('./lib/daily_meeting');
 const sites_blocker = require('./lib/sites_blocker');
 const ora = require('ora');
 const chalk = require('chalk');
@@ -14,19 +16,22 @@ var inquirer = require('inquirer');
 
 //clear();
 (async function () {
-/*     await sites_blocker.checkIfAlreadyBlocked().then(function (result) {
+   /*   await sites_blocker.checkIfAlreadyBlocked().then(function (result) {
         console.log("your result is " + result)
     })
     
-     var hasBeenDone = await code_wars.checkForCodeWarsTasks() */
-    // console.log(hasBeenDone)
-    // await life_goals.showQuestions()
+     var hasBeenDone = await code_wars.checkForCodeWarsTasks()  */
+     await daily_meeting.startDailyMeeting()
+     await life_goals.showQuestions() 
+
+  /*    console.log(hasBeenDone)
     //await send_sms.sendSms()
     /*   await weather.getWeatherApi();
        await joke.printAJoke();
        await mobile.openPhoneAndSwipeToUnlock();
        logo.showRobotFace();*/
-       await mongo.getData()
+      // await mongo.getData()
+     // await todo_table.showTable()
 })();
 
 
