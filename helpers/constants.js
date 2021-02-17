8e040f49ee40bf074034e3667b32845489141deb
mejdi14 <mejdiclubiste@gmail.com>
2021-02-17 15:45:52 +0100

    work with github api

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,7 +1,9 @@
 const localhost_ip = "172.0.0.1"
+const github_username = "mejdi14"
 const mongodb_uri = "mongodb+srv://mejdi14:azma4everfuck14@cluster0.txec9.mongodb.net/test?retryWrites=true&authSource=admin&w=majority"
 module.exports =
 {
     localhost_ip : localhost_ip,
-    mongodb_uri : mongodb_uri
+    mongodb_uri : mongodb_uri,
+    github_username : github_username
 }
