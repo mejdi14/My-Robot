8dd1f7816bdb1ddcc1c79c63c38c7f488967b85c
mejdi14 <mejdiclubiste@gmail.com>
2021-04-12 16:03:54 +0100

    one step closer into music function

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ const anviz = require("node-anviz");
 //clear();
 (async function () {
   logo.showRobotFace();
-  let request = new anviz.Request("192.168.100.17");
+/*   let request = new anviz.Request("192.168.100.17");
 request.execute("getInformation1", 1).on("error", function(err) {
     console.info("ERROR", err);
 }).on("complete", function(res, raw){
     console.info(res, raw);
     request.close();
-});
+}); */
   //await mobile.openPhoneAndSwipeToUnlock();
    //await send_message.openWhatsappAndSendMessage();
-  //await options.showQuestions()
+  await options.showQuestions()
     //await send_sms.sendSms()
     //await github.getGithubEvents()
     // var score = await daily_meeting.startDailyMeeting()
