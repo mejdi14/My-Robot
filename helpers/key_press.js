251fb1ff220e051347d541048c99915527148830
mejdi14 <mejdiclubiste@gmail.com>
2021-09-16 10:50:55 +0100

    hello man now it looks a bit better

--- helpers/key_press.js
+++ helpers/key_press.js
@@ -1,14 +1,17 @@
 var inquirer = require('inquirer');
+var readline = require('readline');
+
 module.exports = {
-    async keypress  () {
-        return new Promise((resolve) => {
-            document.addEventListener('keydown', onKeyHandler);
-            function onKeyHandler(e) {
-              if (e.keyCode === 13) {
-                document.removeEventListener('keydown', onKeyHandler);
+    async keypress() {
+        return new Promise((resolve, reject) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question('press ENTER to go back', (answer) => {
+                rl.close();
                 resolve();
-              }
-            }
-          });
-      }
+            });
+        })
+    }
 };
